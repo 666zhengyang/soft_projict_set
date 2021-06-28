ee3901eb22962cc2a840dae7fae22d01521de1c5
zhengyang <zhengyang@joyliferobot.com>
2021-06-28 19:37:03 +0800

    feat: add readme.md

---
+++ linux_verify/readme.txt
@@ -0,0 +1,4 @@
+1.update samba smb.conf contents.
+2.update vscode vscode.conf contents.
+3.update assemble code contents.
+4.update led code contents.
---
+++ linux_verify/uboot/led_std/imx6ul.h
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: Copyright (C) SHENZHEN JoyLife Robot Co.
+ * @version: v0.01
+ * @Author: zhengyang
+ * @Date: 2021-06-28 16:14:49
+ * @LastEditors: zhengyang
+ * @LastEditTime: 2021-06-28 16:29:50
+ */
+
