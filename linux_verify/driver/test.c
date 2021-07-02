68764fa0aadd504441fb67c70648ea109045fddb
zhengyang <zhengyang@joyliferobot.com>
2021-07-02 18:00:50 +0800

    feat:add the led status

---
+++ linux_verify/driver/test.c
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion: Copyright (C) SHENZHEN JoyLife Robot Co.
+ * @version: v0.01
+ * @Author: zhengyang
+ * @Date: 2021-07-02 17:59:37
+ * @LastEditors: zhengyang
+ * @LastEditTime: 2021-07-02 17:59:38
+ */
