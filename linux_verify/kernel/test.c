ffa31e3067b818a7757408e04f5324803e42d22a
zhengyang <zhengyang@joyliferobot.com>
2021-07-23 14:34:07 +0800

    feat:add the kernel test

--- linux_verify/kernel/test.c
+++
@@ -1,8 +0,0 @@
-/*
- * @Descripttion: Copyright (C) SHENZHEN JoyLife Robot Co.
- * @version: v0.01
- * @Author: zhengyang
- * @Date: 2021-07-02 17:59:50
- * @LastEditors: zhengyang
- * @LastEditTime: 2021-07-02 17:59:50
- */
